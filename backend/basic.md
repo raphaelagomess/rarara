331876664b1d45af55586620bd6a05bb80873e11
Seu Nome <seu_email@example.com>
2024-10-15 20:53:30 -0300

    aula1

---
+++ backend/basic.md
@@ -0,0 +1,4 @@
+# Introdução ao PHP
+##
+- PHP. é uma linguagem de prograação voltada para o 
+desenvolvimento de aplicações web.
---
+++ backend/index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Olá Mundo!";
+
+$acucar = "açucar"; // Variável do tipo string
+$idadeUsuarioAtivoNoSistema = 
+
+?>
